4f67564fd12253612a7fea99772b7347be7ef37b
emreunlu <unlcemre0@gmail.com>
2024-04-21 04:46:49 +0300

    empty project

--- android/app/src/main/kotlin/com/example/flutter_stok_takip/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_stok_takip
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/init/dependencies.dart
+++
@@ -1,6 +0,0 @@
-import 'package:get/get.dart';
-
-class Dependencies extends Bindings {
-  @override
-  void dependencies() {}
-}
